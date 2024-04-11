52051b045095beae15c23d1ac6e9f6997c286f55
gabriel-scaled <gabrieldemoraistaveira@gmail.com>
2024-04-11 20:42:36 -0300

    setup

---
+++ src/main/java/com/gabsleo/meaudote/MeaudoteApplication.java
@@ -0,0 +1,13 @@
+package com.gabsleo.meaudote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeaudoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeaudoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabsleo/meaudote/MeaudoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabsleo.meaudote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeaudoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
